b08522699afca1b8c9b0110d0646a92391797819
landrexxi <becsevangelista@gmail.com>
2021-03-09 09:17:45 +0800

    I added the keyboard type in the amount of category

--- lib/home.dart
+++ lib/home.dart
@@ -97,6 +97,7 @@ class _HomeState extends State<Home> {
                   controller: maxController,
                   decoration: InputDecoration(hintText: 'Maximum Amount'),
                   style: TextStyle(fontWeight: FontWeight.bold),
+                  keyboardType: TextInputType.number,
                 )
               ],
             )),
